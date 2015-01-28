a2fc07dfd7e315593c848963a19f494709df5e16
eg0179 <ericguzman@my.unt.edu>
2015-01-27 19:51:25 -0600

    test

--- app/src/main/java/comjmc05924410project/github/project4410/MainActivity.java
+++ app/src/main/java/comjmc05924410project/github/project4410/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-
+//testing github commit
 public class MainActivity extends ActionBarActivity {
 
     @Override
