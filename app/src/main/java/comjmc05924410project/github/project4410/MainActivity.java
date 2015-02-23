349bd4902d20cf978a4e818f23a3f595fd35e14a
jmc0592 <jmcole17@yahoo.com>
2015-02-22 21:40:16 -0600

    Readded the tab strings

--- app/src/main/java/comjmc05924410project/github/project4410/MainActivity.java
+++ app/src/main/java/comjmc05924410project/github/project4410/MainActivity.java
@@ -105,11 +105,11 @@ public class MainActivity extends ActionBarActivity {
             Locale l = Locale.getDefault();
             switch (position) {
                 case 0:
-                   // return getString(R.string.title_section1).toUpperCase(l);
+                   return getString(R.string.title_section1).toUpperCase(l);
                 case 1:
-                  //  return getString(R.string.title_section2).toUpperCase(l);
+                   return getString(R.string.title_section2).toUpperCase(l);
                 case 2:
-                   // return getString(R.string.title_section3).toUpperCase(l);
+                   return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
         }
