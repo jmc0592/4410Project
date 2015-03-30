929125a16ae75a33dc2aca1213a2c8c77d919787
jmc0592 <jmcole17@yahoo.com>
2015-03-29 19:08:26 -0500

    able to have items added to each list via editText and onClick

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 4410Project
 We are creating an Android application for our software development class. This app is meant to make shopping with multiple people(such as college kids going to the grocery store) much easier. It will display your "shared" items which you want to buy with others together, and your "single" items which you will buy yourself. We plan to implement the cost to pay as well.
 
-Note: The master branch is NOT the branch we will be developing on. We will be developing on the "develop" and "listcapable" branch. In other words, all commits to be seen will be under the "develop" branch.
+Note: The master branch is NOT the branch we will be developing on. We will be developing on the "develop" and "listcapable" branch. In other words, all commits to be seen will be under the "develop" and "listcapable" branch.
 
 eg0179 - Eric Guzman jmc0592 - Jacob Cole msd0113 - Mark Doggendorf
--- app/src/main/java/com/example/shopbroker/CreateListActivity.java
+++ app/src/main/java/com/example/shopbroker/CreateListActivity.java
@@ -2,13 +2,31 @@ package com.example.shopbroker;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateListActivity extends ActionBarActivity {
 
+    //Activity scope variables to handle onClicks
+    private ArrayList<String> itemsShared = new ArrayList<String>();
+    private ArrayList<String> itemsMine = new ArrayList<String>();
+    private int itemSpot = 0;
+    private boolean toAdd = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +38,9 @@ public class CreateListActivity extends ActionBarActivity {
             long rowID = getIntent().getLongExtra("ID", 0);
             String rowID_string = Long.toString(rowID);
 
-
             TextView id = (TextView) findViewById(R.id.editText);
             id.setText(rowID_string);
         }
-
     }
 
 
@@ -48,5 +64,45 @@ public class CreateListActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //onClick for Mine button
+    public void addToMine(View v){
+        String temp;
+        TextView itemET = (TextView) findViewById(R.id.editTextItem);
+        temp = itemET.getText().toString();
+        itemET.setText("");
+        toAdd = true;//test value
+        populateListViewMine(toAdd, temp);
+    }
+
+    //onClick for Shared Button
+    public void addToShared(View v){
+        String temp;
+        TextView itemET = (TextView) findViewById(R.id.editTextItem);
+        temp = itemET.getText().toString();//get text and convert to string
+        itemET.setText("");//reset editText to blank
+        toAdd = true;//test value
+        populateListViewShared(toAdd, temp);
+
+        //more methods and things for updating online database in the future
+    }
+
+    //create list view for Mine
+    public void populateListViewMine(boolean test, String item){
+        if(test)
+            itemsMine.add(item);
+        ListAdapter itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemsMine);
+        ListView listView = (ListView) findViewById(R.id.listViewMine);
+        listView.setAdapter(itemsAdapter);
+    }
+
+    //create list view for shared
+    public void populateListViewShared(boolean test, String item){
+        if(test)
+            itemsShared.add(item);
+        ListAdapter itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemsShared);//convert to list items for ListView
+        ListView listView = (ListView) findViewById(R.id.listViewShared);
+        listView.setAdapter(itemsAdapter);
     }
 }
