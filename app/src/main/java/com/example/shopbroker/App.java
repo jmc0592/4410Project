00366cb51266889ab9949e841531105a77477554
Eric <ericguzman@my.unt.edu>
2015-05-03 15:23:37 -0500

    push notifications

--- app/src/main/java/com/example/shopbroker/App.java
+++ app/src/main/java/com/example/shopbroker/App.java
@@ -1,13 +1,28 @@
 package com.example.shopbroker;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 //needed to resolve app crash
 public class App extends Application{
     public void onCreate() {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this);//uses string values from strings.xml
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 }
